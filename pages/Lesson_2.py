381b4b4fc564ed15863dae4d2344f108c0ea645d
MinhThieu145 <minhthieu135@gmail.com>
2024-02-15 04:19:10 -0500

    Add EC2 instance details to the app

--- pages/Lesson_2.py
+++ pages/Lesson_2.py
@@ -4,10 +4,25 @@ import boto3
 # the title
 st.title('Hey, now is the ELB, let goooo!!!.')
 
+import boto3
 
-import subprocess
+# Create an EC2 resource instance
+ec2 = boto3.resource('ec2')
 
-cmd='''set -o pipefail && sudo grep instance-id /run/cloud-init/instance-data.json | head -1 | sed 's/.*\"i-/i-/g' | sed 's/\",//g\''''    
-status, instance_id = subprocess.getstatusoutput(cmd)
-st.write(status, instance_id)
+# Get the current instance ID
+instance_id = boto3.client('ec2').describe_instances(Filters=[
+    {
+        'Name': 'instance-state-name',
+        'Values': ['running']
+    }
+])['Reservations'][0]['Instances'][0]['InstanceId']
 
+# Use the instance ID to get detailed information
+instance = ec2.Instance(instance_id)
+
+# st.write some details about the instance
+st.write(f"Instance ID: {instance.id}")
+st.write(f"Instance Type: {instance.instance_type}")
+st.write(f"Public IPv4 Address: {instance.public_ip_address}")
+st.write(f"AMI ID: {instance.image_id}")
+st.write(f"State: {instance.state['Name']}")

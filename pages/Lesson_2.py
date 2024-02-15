a7d0d2befd3937efbe6d7815d8552e9b1e20d540
MinhThieu145 <minhthieu135@gmail.com>
2024-02-15 04:08:18 -0500

    Refactor instance ID retrieval in Lesson_2.py

--- pages/Lesson_2.py
+++ pages/Lesson_2.py
@@ -4,16 +4,10 @@ import boto3
 # the title
 st.title('Hey, now is the ELB, let goooo!!!.')
 
-# get the name of the Ec2 that the script is currently running on
-ec2 = boto3.client('ec2')
-response = ec2.describe_instances()
-instance_id = response['Reservations'][0]['Instances'][0]['InstanceId']
 
-st.subheader('Section 1: Instance Information')
+import subprocess
 
-# print the instance id
-st.write(f"Instance ID: {instance_id}")
+cmd='''set -o pipefail && sudo grep instance-id /run/cloud-init/instance-data.json | head -1 | sed 's/.*\"i-/i-/g' | sed 's/\",//g\''''    
+status, instance_id = subprocess.getstatusoutput(cmd)
+st.write(status, instance_id)
 
-# instance name
-instance_name = response['Reservations'][0]['Instances'][0]['Tags'][0]['Value']
-st.write(f"Instance Name: {instance_name}")
